f0e4965abc0ae8ca86fd99df8af5f246348b39b6
madebyporter <general@madebyporter.com>
2025-03-24 17:36:24 -0400

    Update header titles in index.vue and sounds.vue for consistency in branding
    - Changed "Music Production Software" to "Music production software" in index.vue for uniformity in title casing.
    - Updated "Sounds & Kits" to "Music production sounds" in sounds.vue to align with the new branding approach.

--- pages/sounds.vue
+++ pages/sounds.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="col-span-full max-w-full lg:max-w-none p-2 lg:p-0 flex flex-col gap-8 text-neutral-300">
     <header class="pt-8 pb-4 border-b border-neutral-800">
-      <h1 class="text-3xl font-bold indent-1">Sounds & Kits</h1>
+      <h1 class="text-3xl font-bold indent-1">Music production sounds</h1>
     </header>
     <div class="overflow-x-scroll xl:overflow-auto">
       <DatabaseGrid 
